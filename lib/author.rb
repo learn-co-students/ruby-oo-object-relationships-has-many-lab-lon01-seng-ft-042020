54a1455d717653dc7a8a03f00aeac4c673a87fba
jsymondst <jammie@Jammie-XPS.localdomain>
2020-04-27 11:30:32 +0100

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @posts =[]
+        @@all << self
+    end
+
+    def add_post(post)
+        @posts << post
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        new_post = Post.new(title)
+        new_post.author = self
+        self.add_post(new_post)
+    end
+
+    def posts
+        @posts
+    end
+
+
+   def self.post_count
+        Post.all.length
+    end
+
+end
