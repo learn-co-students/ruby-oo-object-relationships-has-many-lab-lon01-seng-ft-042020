4b635ce0b83a3afdd34cd9f965dea5747e135250
Elenadinca25 <elenadinca25@gmail.com>
2020-04-29 14:51:49 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+   
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+  
+    def songs
+        Song.all.select {|song| song.artist = self}
+    end
+
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        singer = Song.new(song_name)
+        add_song(singer)
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+end
